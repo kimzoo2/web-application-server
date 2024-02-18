dc170e53a4741b3be4a4c1b4be8be5bad7a8bd17
kimzoo2 <kimzoo1207@gmail.com>
2024-02-19 01:24:56 +0900

    Test: 요구사항3까지 test

--- src/test/java/util/CustomHttpRequestUtilTest.java
+++ src/test/java/util/CustomHttpRequestUtilTest.java
@@ -17,6 +17,23 @@ public class CustomHttpRequestUtilTest {
 	}
 
 	@Test
+	public void extractRequestHttpMethod(){
+		//get
+		String getHttpHeader = "GET /index.html HTTP/1.1";
+		String reg = " ";
+		String getResult = CustomHttpRequestUtil.parseHttpMethod(getHttpHeader, reg);
+
+		//post
+		String postHttpHeader = "POST /user/form.html HTTP/1.1";
+		String postResult = CustomHttpRequestUtil.parseHttpMethod(postHttpHeader, reg);
+
+		assertThat(getResult).isEqualTo("GET");
+		assertThat(postResult).isEqualTo("POST");
+	}
+
+
+
+	@Test
 	public void extractRequestAccessPath(){
 		String url = "/user/create?userId=javajigi&password=password&name=JaeSung";
 		String[] result = CustomHttpRequestUtil.parseRequestPath(url);
