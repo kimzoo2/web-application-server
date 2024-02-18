3f8f31691bfd2ccada76c28814a4e1984d2fa259
kimzoo2 <kimzoo1207@gmail.com>
2024-02-18 23:27:14 +0900

    Feat: 요구사항2까지 반영

---
+++ src/main/java/util/CustomHttpRequestUtil.java
@@ -0,0 +1,18 @@
+package util;
+
+public class CustomHttpRequestUtil {
+
+	public static String parseURL(String line, String regex) {
+		String[] tokens = line.split(regex);
+		for (String token : tokens) {
+			if (token.startsWith("/"))
+				return token;
+		}
+		return tokens[1];
+	}
+
+	public static String[] parseRequestPath(String url) {
+		String patternString = "\\?"; // /로 시작하고 ?로 끝난다.
+		return url.split(patternString);
+	}
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,24 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.CustomHttpRequestUtil;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -18,19 +29,60 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    @Override
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            List<String> lines = new ArrayList<>();
+            // 요청 헤더를 읽는다.
+            while(true){
+                String line = br.readLine();
+                if((line == null) || line.isEmpty()) break;
+                lines.add(line);
+            }
+            // GET매핑
+            // 요청 헤더에서 url을 추출해서 body를 만든다.
+            String url = extreactRequestURL(lines);
+            String[] urls = CustomHttpRequestUtil.parseRequestPath(url);
+            // 요청 값을 객체에 담는다
+            if(urls.length > 1) {
+                Map<String, String> requestArguments = HttpRequestUtils.parseQueryString(urls[1]);
+                User model = createModel(requestArguments);
+            }
+
+            // POST 매핑 (requestBody에서 데이터 옴)
+            byte[] body = createBody(urls[0]);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Next-Step".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private User createModel(Map<String, String> requestParams){
+        // 하드코딩?
+        return new User(
+            requestParams.get("userId")
+            , requestParams.get("name")
+            , requestParams.get("password")
+            , requestParams.getOrDefault("email", null)
+        );
+    }
+
+    private byte[] createBody(String url) throws IOException {
+        String pathName = "./webapp";
+        log.debug("url : {}", url);
+        return Files.readAllBytes(new File(pathName+url).toPath());
+    }
+
+    private String extreactRequestURL(List<String> lines){
+        return CustomHttpRequestUtil.parseURL(lines.get(0), " ");
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
